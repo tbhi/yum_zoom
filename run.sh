d71dd583a5ff6b67d202df88e709721e8f49c263
Tristan Hill <tristan@me.uk.eu.org>
2020-09-25 07:54:14 +0100

    misc improvements

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
+set -eu
+
+REPO=/var/tmp/yum_zoom
 
 ARCH=x86_64
-REPO=/var/tmp/yum_zoom
 ZOOM_VERSION=$(curl -s -I HEAD https://zoom.us/client/latest/zoom_$ARCH.rpm | sed -n '/^location: / s|.*/prod/\([0-9.]*\)/.*|\1|p')
 TARGET="$REPO/$ARCH/zoom-$ZOOM_VERSION-1.$ARCH.rpm"
 
 if ! [ -f "$TARGET" ]; then
   mkdir -p $(dirname "$TARGET")
   curl -sLo "$TARGET" https://zoom.us/client/$ZOOM_VERSION/zoom_$ARCH.rpm
-  createrepo "$REPO"
+  createrepo -q "$REPO"
 fi
