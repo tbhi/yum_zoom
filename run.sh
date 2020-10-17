6b8561d38f17eb352faa4fd1d275dc57d5716775
Tristan Hill <tristan@me.uk.eu.org>
2020-10-17 10:51:52 +0100

    improve detection if download an invalid file

--- run.sh
+++ run.sh
@@ -7,7 +7,8 @@ ARCH=x86_64
 ZOOM_VERSION=$(curl -s -I HEAD https://zoom.us/client/latest/zoom_$ARCH.rpm | sed -n '/^location: / s|.*/prod/\([0-9.]*\)/.*|\1|p')
 TARGET="$REPO/$ARCH/zoom-$ZOOM_VERSION-1.$ARCH.rpm"
 
-if ! [ -f "$TARGET" ]; then
+if [ "$(file -bi "$TARGET")" != 'application/x-rpm; charset=binary' ]; then
+  rm -rf "$REPO"
   mkdir -p $(dirname "$TARGET")
   curl -sLo "$TARGET" https://zoom.us/client/$ZOOM_VERSION/zoom_$ARCH.rpm
   createrepo -q "$REPO"
